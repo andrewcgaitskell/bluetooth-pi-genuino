e844fbedbc9439986f2ab969a0732b15b8e9e723
andrewcgaitskell <andrew@gaitskell.com>
2021-01-04 15:54:37 +0000

    Update beacon_scan.py

--- beacon_scan.py
+++ beacon_scan.py
@@ -26,7 +26,7 @@ class Beacon(object):
                         MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
         return ret
 
-service = BeaconService("hci0")
+service = BeaconService("LED")
 devices = service.scan(2)
 
 for address, data in list(devices.items()):
